97352704838710545d8f319b846fe2d640769d28
Zabir Al Nazi Nabil <zabiralnazi@codeassign.com>
2017-08-25 00:01:57 +0600

    Added Matlab Code

---
+++ Matlab/video_face_crop.m
@@ -0,0 +1,44 @@
+clc;
+clear all;
+
+% Read Video using VideoReader
+
+videoframe = VideoReader('file_name.mp4');
+
+
+for idx = 1:videoframe.numberofframes
+    
+    curframe = read(videoframe,idx);
+    savedframe = curframe;
+    % figure(1), imshow(curframe), title('Frames');
+ 
+% Face detection using CascadeObjectDetector
+ 
+    uid_prefix = num2str(idx);
+    FD_obj = vision.CascadeObjectDetector; 
+    BB = step(FD_obj, curframe);
+    % figure(2), imshow(curframe), title('Detected Faces');
+
+    for jdx = 1:size(BB,1)
+        rectangle('Position',BB(jdx,:),'LineWidth',3,'LineStyle','-','EdgeColor','r');
+    end
+    
+    if size(BB,1) > 0
+        imwrite(savedframe, strcat('frames/', uid_prefix, 'frame.jpg'),'jpg');
+    end
+
+% crop faces 
+   
+    fdi_idx = 1;
+    for kdx = 1:size(BB,1)
+         Fimg = imcrop(curframe, BB(kdx,:));
+         
+         % figure(3), subplot(2,2,kdx); imshow(Fimg);
+         
+         fdi_idxs = num2str(fdi_idx);
+         imwrite(Fimg, strcat('crops/',uid_prefix, 'crop', fdi_idxs, '.jpg'), 'jpg');
+         fdi_idx = fdi_idx + 1;
+    end
+    
+     
+end
